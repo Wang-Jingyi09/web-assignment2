66ba39f7e3c404edf812f12402ed97ca2d578c95
Jingyi Wang <20104728@email.wit.ie>
2023-12-02 00:34:50 +0000

    Mongo lab Users Router

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 import express from 'express';
 import tasksRouter from './api/tasks';
 import './db';
+//... other imports
+import usersRouter from './api/users';
 
 dotenv.config();
 
@@ -19,9 +21,11 @@ const app = express();
 const port = process.env.PORT;
 app.use(express.json());
 app.use('/api/tasks', tasksRouter);
+app.use('/api/users', usersRouter);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
 });
+//Users router
 
 app.use(errHandler);
